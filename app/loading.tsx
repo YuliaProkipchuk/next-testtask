1cabf9d7ea31de57e4ff8c13b8110297697c9815
YuliaProkipchuk <96261460+YuliaProkipchuk@users.noreply.github.com>
2024-11-25 15:52:55 +0200

    updated code for car dealer app

---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+export default function Loading(){
+    return <div className="h-full w-full flex items-center justify-center">
+        <p className="text-4xl font-bold">Loading...</p>
+    </div>
+}
